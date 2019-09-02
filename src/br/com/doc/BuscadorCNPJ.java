793fa78581b8771fcc7c49b3b4c9aacb7ac5b7ea
Rogerio Siqueira <rogercomp@gmail.com>
2019-09-02 11:16:14 -0300

    Impressão do Objeto JSON Retornado

--- src/br/com/doc/BuscadorCNPJ.java
+++ src/br/com/doc/BuscadorCNPJ.java
@@ -2,17 +2,11 @@ package br.com.doc;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
-import java.util.Iterator;
-import java.util.Map;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
-
-import javax.swing.text.html.HTMLEditorKit.Parser;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -36,7 +30,7 @@ public class BuscadorCNPJ {
 			httpURLConnection.setRequestProperty("Accept", "application/json");
 			httpURLConnection.setRequestMethod("GET");
 			if (200 <= httpURLConnection.getResponseCode() && httpURLConnection.getResponseCode() <= 299) {
-				BufferedReader br = new BufferedReader(new InputStreamReader((httpURLConnection.getInputStream())));
+				BufferedReader br = new BufferedReader(new InputStreamReader((httpURLConnection.getInputStream()), StandardCharsets.UTF_8));
 				StringBuilder sb = new StringBuilder();
 				String output;
 				while ((output = br.readLine()) != null) {
@@ -53,15 +47,58 @@ public class BuscadorCNPJ {
 
 					// Atividade Principal
 					JSONArray atividade = jsonObject.getJSONArray("atividade_principal");
+					String ativPrinc = "";
 					for (int j = 0; j < atividade.length(); j++) {
 						JSONObject p = (JSONObject) atividade.get(i);
-						String ativ = p.getString("text");
-						pj.setAtividadePrincipal(ativ);
-					}					
+						String ativ = p.getString("code") + " " + p.getString("text");
+						ativPrinc += ativ;
+					}
+					
+					pj.setAtividadePrincipal(ativPrinc);					
+					pj.setAbertura(jsonObject.getString("data_situacao") );
+					pj.setNome(jsonObject.getString("nome"));
+					pj.setUf(jsonObject.getString("uf"));
+					pj.setTelefone(jsonObject.getString("telefone"));					
+					
+					// Atividade Secundaria
+					JSONArray atividadeSec = jsonObject.getJSONArray("atividades_secundarias");
+					String ativSec = "";
+					for (int j = 0; j < atividadeSec.length(); j++) {
+						JSONObject p = (JSONObject) atividadeSec.get(i);
+						String ativ = p.getString("code") + " " + p.getString("text");
+						//pj.setAtividadeSecundaria((j + 1) == (atividadeSec.length()) ? ativ : ativ + "\n");
+						ativSec +=  (j + 1) == (atividadeSec.length()) ? ativ : ativ + "\n";
+					}
+					
+					pj.setAtividadeSecundaria(ativSec);					
+					pj.setSituacao(jsonObject.getString("situacao"));
+					pj.setBairro(jsonObject.getString("bairro"));
+					pj.setLogradouro(jsonObject.getString("logradouro"));
+					pj.setNumero(jsonObject.getString("numero"));
+					pj.setCep(jsonObject.getString("cep"));
+					pj.setMunicipio(jsonObject.getString("municipio"));
+					pj.setDataSitucao(jsonObject.getString("data_situacao"));
+					pj.setFantasia(jsonObject.getString("fantasia"));
+					pj.setCnpj(jsonObject.getString("cnpj"));
+					pj.setStatus(jsonObject.getString("status"));
 					
 					//Imprimindo retorno do json
 					System.out.println(pj.getAtividadePrincipal());
-					
+					System.out.println(pj.getDataSitucao());
+					System.out.println(pj.getNome());
+					System.out.println(pj.getUf());
+					System.out.println(pj.getTelefone());
+					System.out.println(pj.getAtividadeSecundaria());
+					System.out.println(pj.getSituacao());
+					System.out.println(pj.getBairro());
+					System.out.println(pj.getLogradouro());
+					System.out.println(pj.getNumero());
+					System.out.println(pj.getCep());
+					System.out.println(pj.getMunicipio());					
+					System.out.println(pj.getAbertura());
+					System.out.println(pj.getFantasia());
+					System.out.println(pj.getCnpj());
+					System.out.println(pj.getStatus());
 					
 				}
 				// System.out.println(json.toString());
--- src/br/com/doc/PessoaJuridica.java
+++ src/br/com/doc/PessoaJuridica.java
@@ -3,13 +3,148 @@ package br.com.doc;
 public class PessoaJuridica {
 
 	private String atividadePrincipal;
+	private String dataSitucao;
+	private String nome;
+	private String uf;
+	private String telefone;
+	private String atividadeSecundaria;
+	private String situacao;
+	private String bairro;
+	private String municipio;
+	private String logradouro;
+	private String numero;
+	private String cep;
+	private String abertura;
+	private String fantasia;
+	private String cnpj;
+	private String status;
+
+	public String getStatus() {
+		return "Status: " + status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getDataSitucao() {
+		return "Data Situação: " + dataSitucao;
+	}
+
+	public void setDataSitucao(String dataSitucao) {
+		this.dataSitucao = dataSitucao;
+	}
+
+	public String getNome() {
+		return "Nome: " + nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getUf() {
+		return "UF: " + uf;
+	}
+
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+
+	public String getTelefone() {
+		return "Telefone: " + telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getAtividadeSecundaria() {
+		return "Atividade(s) Secundária(s): " + atividadeSecundaria;
+	}
+
+	public void setAtividadeSecundaria(String atividadeSecundaria) {
+		this.atividadeSecundaria = atividadeSecundaria;
+	}
+
+	public String getSituacao() {
+		return "Situação: " + situacao;
+	}
+
+	public void setSituacao(String situacao) {
+		this.situacao = situacao;
+	}
+
+	public String getBairro() {
+		return "Bairro: " + bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getMunicipio() {
+		return "Municipio: " + municipio;
+	}
+
+	public void setMunicipio(String municipio) {
+		this.municipio = municipio;
+	}
+
+	public String getLogradouro() {
+		return "Logradouro: " + logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public String getNumero() {
+		return "Número: " + numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getCep() {
+		return "CEP: " + cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getAbertura() {
+		return "Data Abertura: " + abertura;
+	}
+
+	public void setAbertura(String abertura) {
+		this.abertura = abertura;
+	}
+
+	public String getFantasia() {
+		return "Fantasia: " + fantasia;
+	}
+
+	public void setFantasia(String fantasia) {
+		this.fantasia = fantasia;
+	}
+
+	public String getCnpj() {
+		return "CNPJ: " + cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
 
 	public String getAtividadePrincipal() {
-		return atividadePrincipal;
+		return "Atividade Principal: " + atividadePrincipal;
 	}
 
 	public void setAtividadePrincipal(String atividadePrincipal) {
-		this.atividadePrincipal = "Atividade: " + atividadePrincipal;
+		this.atividadePrincipal =  atividadePrincipal;
 	}
-	
+
 }
